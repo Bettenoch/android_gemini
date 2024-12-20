9ac3dca8e3cc7a574411dae44cf7e0b075d141a6
Bettenoch <bettenoch254@gmail.com>
2024-12-20 18:49:58 +0300

    Modify main file

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter WIth vertex ai',
       theme: ThemeData(
         // This is the theme of your application.
         //
